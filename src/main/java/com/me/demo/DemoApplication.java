55559edf7fe8363f9a8733eb369db7ba4dfdc802
shayaan.munshi <shayaan.munshi@eclinicalworks.com>
2019-01-07 17:07:52 -0500

    Demo init

---
+++ src/main/java/com/me/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.me.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
